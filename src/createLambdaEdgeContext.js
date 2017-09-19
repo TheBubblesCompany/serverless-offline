e93e6a36d7f080e444f83e0a6d8f4f5ca923fbda
chreble <christopheeble@gmail.com>
2017-09-19 21:24:26 +0200

    fixup! Add Lambda Edge support

--- src/createLambdaEdgeContext.js
+++ src/createLambdaEdgeContext.js
@@ -1,4 +1,7 @@
-"use strict";
+'use strict';
+
+const _ = require('lodash');
+
 /*
  Mimicks the request event object
  http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
@@ -8,21 +11,32 @@ module.exports = function createLambdaEdgeContext(
   options,
   stageVariables
 ) {
+  const headers = {};
+
+  Object.keys(request.headers).forEach(k => {
+    headers[k] = [
+      {
+        key: _.startCase(_.toLower(k)).replace(' ', '-'),
+        value: request.headers[k],
+      },
+    ];
+  });
+
   return {
     Records: [
       {
         cf: {
           config: {
-            distributionId: "EDFDVBD6EXAMPLE"
+            distributionId: 'EDFDVBD6EXAMPLE',
           },
           request: {
             clientIp: request.connection.remoteAddress,
             method: request.method.toUpperCase(),
             uri: request.route.path,
-            headers: request.headers
-          }
-        }
-      }
-    ]
+            headers,
+          },
+        },
+      },
+    ],
   };
 };
